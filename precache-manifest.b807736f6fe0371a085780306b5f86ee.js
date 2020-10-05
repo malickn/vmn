0f4d12692a7b1a937c1110300c790f3dbad6878d
Malick Ndoye <malickn@vmn.net>
2020-10-05 13:59:41 -0400

    Updates

---
+++ precache-manifest.b807736f6fe0371a085780306b5f86ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463c267ab757e712f4ff99e1308635ca",
+    "url": "/malickn/vmn/index.html"
+  },
+  {
+    "revision": "786650ecfad52da120c3",
+    "url": "/malickn/vmn/static/css/main.01220a70.chunk.css"
+  },
+  {
+    "revision": "72bf8d08f0e2d21f00e1",
+    "url": "/malickn/vmn/static/js/2.caaeb66b.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/malickn/vmn/static/js/2.caaeb66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786650ecfad52da120c3",
+    "url": "/malickn/vmn/static/js/main.3cfea88b.chunk.js"
+  },
+  {
+    "revision": "b2ffc3f15597a500aac8",
+    "url": "/malickn/vmn/static/js/runtime-main.50bc9035.js"
+  }
+]);
